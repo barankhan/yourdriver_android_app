46d460ca4dec84fadc83cc1e86bd24012a9c9590
Baran Khan <selfcoordinator@gmail.com>
2019-11-30 19:10:35 +0500

    Layout Fixed of Driver Data

--- app/src/main/java/com/baran/driver/Fragments/DriverDataUpdateFragment.java
+++ app/src/main/java/com/baran/driver/Fragments/DriverDataUpdateFragment.java
@@ -15,29 +15,22 @@ import com.baran.driver.R;
 
 
 public class DriverDataUpdateFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 
     public DriverDataUpdateFragment() {
         // Required empty public constructor
     }
 
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_driver_data_update, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_driver_data_update, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
+
+        return view;
+    }
 
 
 }
