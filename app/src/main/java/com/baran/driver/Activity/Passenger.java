9fe57e558b7b25f5fc449e676f88dae4d0b36652
Baran Khan <selfcoordinator@gmail.com>
2019-12-03 19:17:20 +0500

    fic

--- app/src/main/java/com/baran/driver/Activity/Passenger.java
+++ app/src/main/java/com/baran/driver/Activity/Passenger.java
@@ -4,9 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.baran.driver.Extras.AppPreference;
+import com.baran.driver.Fragments.passenger.gallery.GalleryFragment;
+import com.baran.driver.Fragments.passenger.home.HomeFragment;
+import com.baran.driver.Fragments.passenger.logout.LogoutFragment;
 import com.baran.driver.R;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,20 +26,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class Passenger extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     NavigationView navigation;
     public static AppPreference appPreference;
-
+    ActionBarDrawerToggle action;
+    Button btnDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        btnDrawerToggle = findViewById(R.id.button2);
         appPreference = new AppPreference(this);
 
         final DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -49,7 +60,70 @@ public class Passenger extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
+        action = new ActionBarDrawerToggle(this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+
+            /* Called when drawer is closed */
+            public void onDrawerClosed(View view) {
+                //Put your code here
+            }
+
+            /* Called when a drawer is opened */
+            public void onDrawerOpened(View drawerView) {
+                //Put your code here
+            }
+        };
+
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem,drawer);
+                        return true;
+                    }
+                });
     }
+
+
+    public void selectDrawerItem(MenuItem menuItem,DrawerLayout drawer) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass = null;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_home:
+                fragmentClass = HomeFragment.class;
+                break;
+            case R.id.nav_gallery:
+                fragmentClass = GalleryFragment.class;
+                break;
+            case R.id.nav_logout:
+                fragmentClass = LogoutFragment.class;
+                break;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.getFragments().clear();
+        fragmentManager.beginTransaction().replace(R.id.nav_host_fragment,fragment).commit();
+
+
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        drawer.closeDrawers();
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -70,11 +144,5 @@ public class Passenger extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
     }
-
-
-
-
-
-
 
 }
